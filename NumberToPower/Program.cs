87f9105cc53d3ffd1dfaca658428bf1045156220
seda <seda@MacBook-Pro-seda.local>
2020-11-13 12:16:03 +0600

    hw

---
+++ NumberToPower/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NumberToPower
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input power: ");
+            int power = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result = result * num;
+            }
+
+            Console.WriteLine("Number to Power => " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
